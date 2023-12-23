c560ce8089bf97d048c41f81c64270c4fbaef807
Francesco Banterle <frabante@gmail.com>
2023-12-23 21:44:10 +0200

    Update nor-vdpnetpp.py

--- nor-vdpnetpp.py
+++ nor-vdpnetpp.py
@@ -16,33 +16,34 @@ from qmodel import QModel
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='NoR-VDPNet++', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('run', type=str, help='Application [tonemapping|inversetonemapping|hdr_compression|sdr_distortion')
+    parser.add_argument('mode', type=str, help='Application [tonemapping|inversetonemapping|hdr_compression|sdr_distortion')
     parser.add_argument('data', type=str, help='Base dir of run to evaluate')
 
     args = parser.parse_args()
 
-    if args.run == 'tonemapping':
+    if args.mode == 'tonemapping':
         checkpoint = 'weights/weight_tmo'
 
-    if args.run == 'inversetonemapping':
+    if args.mode == 'inversetonemapping':
         checkpoint = 'weights/weight_itmo'
 
-    if args.run == 'hdr_compression':
+    if args.mode == 'hdr_compression':
         checkpoint = 'weights/weight_hdrc'
 
-    if args.run == 'sdr_distortion':
+    if args.mode == 'sdr_distortion':
         checkpoint = 'weights/weight_sdr'
         
     model = QModel(checkpoint, 2, 1400, (args.grayscale == 1))
     
     #run for each file
-    names_hdr = [f for f in os.listdir(args.data) if f.endswith('.hdr')]
-    names_exr = [f for f in os.listdir(args.data) if f.endswith('.exr')]
+    names_hdr = [f for f in os.listdir(args.data) if f.lower().endswith('.hdr')]
+    names_exr = [f for f in os.listdir(args.data) if f.lower().endswith('.exr')]
 
-    names_jpg = [f for f in os.listdir(args.data) if f.endswith('.jpg')]
-    names_png = [f for f in os.listdir(args.data) if f.endswith('.png')]
+    names_jpg = [f for f in os.listdir(args.data) if f.lower().endswith('.jpg')]
+    names_jpeg = [f for f in os.listdir(args.data) if f.lower().endswith('.jpeg')]
+    names_png = [f for f in os.listdir(args.data) if f.lower().endswith('.png')]
 
-    names = names_hdr + names_exr + names_jpg + names_png
+    names = names_hdr + names_exr + names_jpg + names_jpeg + names_png
     
     for name in names:
         fn = os.path.join(args.data, name)
